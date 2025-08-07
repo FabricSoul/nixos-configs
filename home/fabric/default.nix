099709c3c2746c6b7d777b258924d6d72345e3ec
FabricSoul <fabric.soul7@gmail.com>
2025-08-07 19:15:10 -0400

    update: home, dwl

--- home/fabric/default.nix
+++ home/fabric/default.nix
@@ -1,6 +1,5 @@
 {
   nixvim,
-  zen-browser,
   # hyprpanel,
   pkgs,
   pkgsUnstable,
@@ -11,6 +10,7 @@
     # Import program configurations
     ./programs/git.nix
     ./programs/kitty.nix
+    ./programs/foot.nix
     ./programs/nixvim.nix
     ./programs/tmux.nix
     ./programs/zsh.nix
@@ -18,12 +18,14 @@
     ./programs/yazi.nix
 
     # Import desktop configurations
-    ./desktop/hyprland.nix
-    ./desktop/hyprpanel.nix
-    ./desktop/hyprpaper.nix
-    ./desktop/hypridle.nix
+    # ./desktop/hyprland.nix
+    # ./desktop/hyprpanel.nix
+    # ./desktop/river.nix
+    # ./desktop/hyprpaper.nix
+    # ./desktop/hypridle.nix
     ./desktop/desktop.nix
-    zen-browser.homeModules.beta
+    ./desktop/waybar.nix
+    ./desktop/mako.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should manage
@@ -41,7 +43,6 @@
   nixpkgs.config = {
     allowUnfree = true;
   };
-  programs.zen-browser.enable = true;
 
   # Common packages used across your system
   home.packages = with pkgs; [
@@ -49,94 +50,37 @@
     lazygit
     kitty
     firefox
-    zen-browser
     mpv
     grim
     slurp
     wlsunset
     newsraft
     ghostty
+    wlr-randr
+    wmenu
+    foot
+    qutebrowser
+    ffmpeg
+    android-tools
+    wbg
+    emacs
+    ripgrep
+    fd
+    lsd
+    pulseaudio
 
-    nerd-fonts.dejavu-sans-mono
-    nerd-fonts.daddy-time-mono
-    nerd-fonts.d2coding
-    nerd-fonts.cousine
-    nerd-fonts.commit-mono
-    nerd-fonts.comic-shanns-mono
-    nerd-fonts.code-new-roman
-    nerd-fonts.caskaydia-mono
-    nerd-fonts.caskaydia-cove
-    nerd-fonts.blex-mono
-    nerd-fonts.bitstream-vera-sans-mono
-    nerd-fonts.bigblue-terminal
-    nerd-fonts.aurulent-sans-mono
-    nerd-fonts.arimo
-    nerd-fonts.anonymice
-    nerd-fonts.agave
-    nerd-fonts._3270
-    nerd-fonts._0xproto
-    nerd-fonts.zed-mono
-    nerd-fonts.victor-mono
-    nerd-fonts.ubuntu-sans
-    nerd-fonts.ubuntu-mono
-    nerd-fonts.ubuntu
-    nerd-fonts.tinos
-    nerd-fonts.terminess-ttf
-    nerd-fonts.symbols-only
-    nerd-fonts.space-mono
-    nerd-fonts.shure-tech-mono
-    nerd-fonts.sauce-code-pro
-    nerd-fonts.roboto-mono
-    nerd-fonts.recursive-mono
-    nerd-fonts.proggy-clean-tt
-    nerd-fonts.profont
-    nerd-fonts.overpass
-    nerd-fonts.open-dyslexic
-    nerd-fonts.noto
-    nerd-fonts.mononoki
-    nerd-fonts.monoid
-    nerd-fonts.monofur
-    nerd-fonts.monaspace
-    nerd-fonts.meslo-lg
-    nerd-fonts.martian-mono
-    nerd-fonts.lilex
-    nerd-fonts.liberation
-    nerd-fonts.lekton
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.iosevka-term-slab
-    nerd-fonts.iosevka-term
-    nerd-fonts.iosevka
-    nerd-fonts.intone-mono
-    nerd-fonts.inconsolata-lgc
-    nerd-fonts.inconsolata-go
-    nerd-fonts.inconsolata
-    nerd-fonts.im-writing
-    nerd-fonts.hurmit
-    nerd-fonts.heavy-data
-    nerd-fonts.hasklug
-    nerd-fonts.hack
-    nerd-fonts.gohufont
-    nerd-fonts.go-mono
-    nerd-fonts.geist-mono
     nerd-fonts.fira-mono
-    nerd-fonts.fira-code
-    nerd-fonts.fantasque-sans-mono
-    nerd-fonts.envy-code-r
-    nerd-fonts.droid-sans-mono
-    nerd-fonts.departure-mono
-    font-awesome
 
     wl-clipboard
 
     tmux
-    discord
     fzf
     zsh
     pavucontrol
     wofi
-    # hyprpanel
     alejandra
     rustup
+    zig
     tmuxPlugins.sensible
     tmuxPlugins.vim-tmux-navigator
     tmuxPlugins.onedark-theme
@@ -145,13 +89,10 @@
     go
     go-blueprint
     gccgo14
-    hypridle
     dprint
-    code-cursor
     delve
     ldtk
     libresprite
-    gh
     btop
     leetgo
     gnumake
@@ -179,8 +120,6 @@
     psmisc
     air
     pyfa
-
-    google-chrome
   ];
 
   # Enable home-manager
