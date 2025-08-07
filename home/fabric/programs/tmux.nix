86acc4b8bdd07a4a699a897418195fea12afa9a5
FabricSoul <fabric.soul7@gmail.com>
2025-08-07 19:14:47 -0400

    update: config

--- home/fabric/programs/tmux.nix
+++ home/fabric/programs/tmux.nix
@@ -1,14 +1,9 @@
 # home/fabric/programs/tmux.nix
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     plugins = with pkgs.tmuxPlugins; [
       sensible
-      onedark-theme
       vim-tmux-navigator
     ];
 
@@ -19,14 +14,46 @@
 
     # Custom key bindings
     extraConfig = ''
-      # Set pane base index
       setw -g pane-base-index 1
+
 
       # Vi copy mode bindings
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "wl-copy"
       bind-key P paste-buffer
       bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "wl-copy"
+      set-option -g status-position top
+
+      set -g status-style 'fg=#CDD6F4,bg=default'
+      set -g status-justify centre # Center the window list
+
+      # Remove anything from the left side
+      set -g status-left ' '
+
+
+      # --- WINDOW LIST (CENTERED) ---
+      # Set the style for the active window
+      # We use a bright blue from your theme and make it bold
+      setw -g window-status-current-style 'fg=#89B4FA,bold'
+      # Format for the active window, using a Nerd Font icon
+      setw -g window-status-current-format '  #I-#W '
+
+      # Set the style for inactive windows
+      # We use a muted gray color from your theme
+      setw -g window-status-style 'fg=#585B70'
+      # Format for inactive windows, using a different Nerd Font icon
+      setw -g window-status-format '  #I-#W '
+
+
+      # --- RIGHT SIDE ---
+      # Set the content for the right side of the status bar
+      # 󰒋 is a Nerd Font icon for a computer/host
+      # #H is the placeholder for the hostname
+      set -g status-right ' 󰒋 #H '
+      # Set the max length for the right status bar
+      set -g status-right-length 40
+
+
     '';
   };
 }
