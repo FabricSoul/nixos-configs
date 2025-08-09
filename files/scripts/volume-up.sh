99e72d204dc044637108777ea71bb54d8fcad905
FabricSoul <fabric.soul7@gmail.com>
2025-08-09 14:25:58 -0400

    scripts for solaris

---
+++ files/scripts/volume-up.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "Running volume-up.sh at $(date)" >> /tmp/dwl-script.log
+echo "USER=$USER" >> /tmp/dwl-script.log
+echo "PATH=$PATH" >> /tmp/dwl-script.log
+exec pactl set-sink-volume @DEFAULT_SINK@ +5%
