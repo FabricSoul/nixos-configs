88b599c24489f57985d9738f11d921890b82f3cd
FabricSoul <fabric.soul7@gmail.com>
2025-08-12 14:43:08 -0400

    feat: add jellyfin and navidrome

--- hosts/common/optional/plex.nix
+++ hosts/common/optional/plex.nix
@@ -3,4 +3,16 @@
     enable = true;
     openFirewall = true;
   };
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+  services.navidrome = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      MusicFolder = "/nas/music";
+      Address = "0.0.0.0";
+    };
+  };
 }
